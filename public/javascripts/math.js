70b946a56e7a0628be3dc832b51cf1783786e4b7
Nicolas Riesco <riesco@gmail.com>
2013-06-30 00:58:55 -0400

    comments

--- public/javascripts/math.js
+++ public/javascripts/math.js
@@ -1,11 +1,14 @@
 
 /*
-* increments a number by 100
+* increments a number by 1
 */
 exports.increment = function(number) {
   return number + 1;
 }
 
+/*
+* adds two numbers
+*/
 exports.add = function(number1, number2) {
   return number1 + number2;
 }
--- public/javascripts/mathFail.js
+++ public/javascripts/mathFail.js
@@ -1,11 +1,19 @@
 
 /*
+* contains the methods implemented in math
+* method's names are the same on purpose
+*/
+
+/*
 * increments a number by 100
 */
 exports.increment = function(number) {
   return number + 100;
 }
 
+/*
+* adds two numbers, but the second is first multiplied by 100
+*/
 exports.add = function(number1, number2) {
   return number1 + number2 * 100;
 }

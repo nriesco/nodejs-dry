daa081e98e1708c7a9603f606ff7d8958b2735f9
Nicolas Riesco <riesco@gmail.com>
2013-06-30 01:08:40 -0400

    more tests

--- tests/basic.js
+++ tests/basic.js
@@ -28,16 +28,36 @@ suite('math', function(){
       assert.equal(2, math.add(1, 1));
       assert.equal(0, math.add(4, -4));
       assert.equal(0, math.add(-4, 4));
+      assert.equal(0, math.add(0, 0));
       assert.equal(4, math.add(4, 0));
       assert.equal(4, math.add(0, 4));
     });
+    test('non integers', function(){
+      assert.equal(1434.53423, math.add(1.23423, 1433.3));
+      assert.equal(1, math.add(1/3, 2/3));
+      assert.equal(0, math.add(54533.23235254, -54533.23235254));
+    });
   });
 });
+
+
 
 suite('mathFail', function(){
   setup(function(){
     // ...
   });
 
+  suite('#increment(number)', function(){
+    test('basic increments', function(){
+      assert.equal(100, mathFail.increment(0));
+    });
+  });
+
+  suite('#add(number1, number2)', function(){
+    test('basic', function(){
+      assert.equal(101, mathFail.add(1, 1));
+      assert.equal(-396, mathFail.add(4, -4));
+    });
+  });
 });
 
